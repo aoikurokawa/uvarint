61794d5b3bf6198a2e481b7a83f2189574d1a81b
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-10-20 16:27:27 +0900

    Implement IO (#11)
    * feat: io
    
    * docs: remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
-pub mod decode;
-pub mod encode;
-pub mod error;
+mod decode;
+mod encode;
+mod error;
+pub mod io;
+
+pub use decode::{decode_u32, decode_u64, decode_u128};
+pub use encode::{
+    encode_u32, encode_u32_into, encode_u64, encode_u64_into, encode_u128, encode_u128_into,
+};
+pub use error::UVarintError;
